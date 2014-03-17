fa1ee55ff3f05bac3913d8b4da9d7445aa445569
Nick Elsbree <renegadelsbree@gmail.com>
2014-03-16 17:21:22 -0700

    Made css styles work.

--- sdk/js/sdk.js
+++ sdk/js/sdk.js
@@ -16,6 +16,20 @@ require.config({
 define(['player'], function(player) {
     ToneDen.ready = true;
 
+    // Inject CSS into the dom.
+    var style = document.createElement('style');
+    style.type = 'text/css';
+    css = css.replace(/\}/g, "}\n");
+
+    if (style.styleSheet) {
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+
+    var entry = document.getElementsByTagName('script')[0];
+    entry.parentNode.insertBefore(style, entry);
+
     return {
         player: player
     };
