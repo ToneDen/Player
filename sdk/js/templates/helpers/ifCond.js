491c590db772e45dbd294439b61057b64ef75e77
Tim Thimmaiah <tim.thimmaiah@gmail.com>
2014-03-27 18:17:12 -0700

    changes

---
+++ sdk/js/templates/helpers/ifCond.js
@@ -0,0 +1,12 @@
+define(['hbs/handlebars'], function(Handlebars) {
+	function ifCond(v1, v2, options) {
+		if(v1 === v2) {
+	    return options.fn(this);
+	  }
+	  return options.inverse(this);
+	}
+
+	Handlebars.registerHelper('ifCond', ifCond);
+
+	return ifCond;
+});
