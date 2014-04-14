49dfc78511d006f227cdaf97355e5a4ed8155287
Tim Thimmaiah <tim.thimmaiah@gmail.com>
2014-04-14 03:12:29 -0700

    fixed helper

--- sdk/js/templates/helpers/commanator.js
+++ sdk/js/templates/helpers/commanator.js
@@ -2,6 +2,8 @@ define(['hbs/handlebars'], function(Handlebars) {
     function commanator(num) {
     	if(num) {
         	return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+        } else {
+        	return '-';
         }
     }
 
